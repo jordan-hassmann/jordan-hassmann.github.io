25fc5a254827580b6243cf1f24871cf602fd7fe7
jhassmann <jhassmann@tamu.edu>
2022-12-07 17:26:20 -0600

    Another Scroll Fix

--- JavaScript/SideNavigation.js
+++ JavaScript/SideNavigation.js
@@ -4,7 +4,9 @@ const hamburger = document.querySelector('.mobile-nav')
 const close = document.querySelector('.side-nav .close')
 const sideNav = document.querySelector('.side-nav')
 const links = document.querySelectorAll('.side-nav .link')
+const app = document.querySelector('app')
 const body = document.querySelector('body')
+const html = document.querySelector('html')
 const pages = [
     '#home', 
     '#projects', 
@@ -15,12 +17,16 @@ const pages = [
 
 hamburger.addEventListener('click', e => {
     sideNav.style.right = '0vw'
+    app.style.overflow = 'hidden'
     body.style.overflow = 'hidden'
+    html.style.overflow = 'hidden'
 })
 
 close.addEventListener('click', e => {
     sideNav.style.right = '-100vw'
+    app.style.overflow = 'scroll'
     body.style.overflow = 'scroll'
+    html.style.overflow = 'scroll'
 })
 
 links.forEach(link => {
